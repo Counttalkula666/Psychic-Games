05ad775198914c41186dc9b18e1c4dfd782cb6bf
Counttalkula666 <elenabelgum@gmail.com>
2018-08-02 21:09:34 -0700

    almost completed js

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,11 +15,59 @@
 
 //this defines the variable "alphabet" and also assigns the value of the alphabet variable as an array of all the letters in the alphabet
 
-var alphabet = ['a','b', 'c','d','e','f','g','h','i','j','k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't','u','v','w','x','y','z'];
+var alphabet = ['a','b', 'c','d','e','f','g','h','i','j','k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't','u','v',
+'w','x','y','z'];
+var wins = 0;
+var losses = 0;
+var guesses = 5;
 
+var compGuess = alphabet[Math.floor(Math.random() * alphabet.length)];
 // this picks our random letter (comp pick)
-console.log(alphabet[Math.floor(Math.random() * alphabet.length)]);
+console.log('computer guess', compGuess);
+var someObj = {
+    someKey: 2,
+    another: '3',
+    someArray:[1,2,3,4,5,6],
+    anotherOne: {nested: 'keys'}
+}
+//How do we get input from the user
+//We need to access the document to access it s methods
+document.onkeydown = function(event){
+console.log(event.key);
+//indexOf it is tagged onto an array and acts on that array by looking in the array for the value passed in
+//if found it returns the vlaid index position. If not in array it returns -1.
+var userGuess = event.key;
+var validGuess = validateUserInput(userGuess);
 
-console.log(Math.floor(Math.random() * alphabet.length));
-console.log(alphabet.length)
+
+if(validGuess && userGuess === compGuess) {
+    wins++; //wins = wins + 1;
+    guesses = 5;
+    //Need to implement if else statement checking playAgain variable Y or N for playing again
+   var playAgain = prompt('You guessed correctly! Want to play again? Y or N');
+console.log(playAgain);
+
+//If Y restart game ELSE iF N close window ELSE Prompt again because that is not a valid response
+
+}
+else if (validGuess && userGuess !== compGuess){
+   guesses--; //guesses = guesses - 1;
+   //check if guess it 0 then they lose
+   alert('No that was wrong you only have ' + guesses + " guesses left! Try again");
+}
+else {
+    //user did not give us good input
+    alert('bad input')
+}
+};
+
+function validateUserInput(userInput) {
+    if(alphabet.indexOf(userInput) !== -1) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
 
